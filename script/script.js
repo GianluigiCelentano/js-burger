7c10e979917d85135ea1cf9831de6c8b1d913bb4
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-07 16:52:18 +0200

    js-burger

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 var calculateButton = document.getElementsByClassName("calculate")[0];
-var totalCost = document.getElementById("total")
+var totalCost = document.getElementById("sum-ingredients")
 calculateButton.addEventListener('click', function(){
+    var result = 0
     var ingredients = document.getElementsByClassName("ingredients");
     for (var i = 0; i < ingredients.length; i++) {
         if(ingredients[i].checked) {
             console.log(ingredients[i].value)
-            totalCost.innerHTML = ingredients[i].value
+            result += parseInt(ingredients[i].value)
         }
     }
+    totalCost.innerHTML += " " + result
 })
