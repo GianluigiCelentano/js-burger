3d460faa9eb20090ab9777421cd493792417710b
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-07 16:23:55 +0200

    js-burger

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
 var calculateButton = document.getElementsByClassName("calculate")[0];
+var totalCost = document.getElementById("total")
+calculateButton.addEventListener('click', function(){
+    var ingredients = document.getElementsByClassName("ingredients");
+    for (var i = 0; i < ingredients.length; i++) {
+        if(ingredients[i].checked) {
+            console.log(ingredients[i].value)
+            totalCost.innerHTML = ingredients[i].value
+        }
+    }
+})
